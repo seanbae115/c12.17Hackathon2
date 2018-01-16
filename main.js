776299dd341da8a1fb3ad64da435c26107177712
Sean Bae <seanbae115@gmail.com>
2018-01-16 13:18:17 -0800

    getTop9Artists and renderArtist function added

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ $.ajax({
 	error: function(response) {
 		console.log(response);
 	}
-})
+});
 
 /**
 * Code to run when document has loaded
@@ -35,4 +35,34 @@ $(document).ready(function() {
 
 });
 //-------------------------------------------------------------------------------
+//var artists = [];
+/*function getTop9Artists(user) {
+	$.ajax({
+		dataType: 'json',
+		url: 'https://api.spotify.com/v1/user/top/artists',
+		limit: 9,
+		method: 'GET',
+		success: function (response) {
+			console.log(response);
+        }
+    });
+}*/
+
+/*
+function renderArtists(artists) {
+	for(var artistIndex = 0; artistIndex < artist.length; artistIndex++){
+        var name = artist[artistIndex].name;
+        var imageUrl = artist[artistIndex].image[2].url;
+        var img = $('<img>',{
+            src: imageUrl,
+			'class': 'rounded-circle border border-primary img-responsive w-100'
+        });
+        var nameDiv = $('<div>',{
+        	text: name,
+			'class': 'text-center caption'
+		});
+
+	}
+
+}*/
 
