db83329dcfae9223c7c7b5532e8bae0c8e883546
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-01-16 12:00:09 -0800

    testing spotify api

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 //-------------------------------------------------------------------------------
 // start document
 //-------------------------------------------------------------------------------
+// var spotifyApi = new SpotifyWebApi();
+
+// spotifyApi.setAccessToken('AQCYrCIDSLYbQSfMRGISNh-VquWKE-P_qF3NyerO3lz25bxRoo_5n00nRrBK3_f9IfjWv1clNHZULfM2')
+
+// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+//   if (err) console.error(err);
+//   else console.log('Artist albums', data);
+// });
+$.ajax({
+	method: "GET",
+	url: "https://accounts.spotify.com/authorize?client_id=514d4820fc1c42eb9e18c9d9ea28de53&redirect_uri=http:%2F%2Flocalhost:8888%2Fcallback&response_type=token",
+	success: function(response) {
+		console.log(response);
+	},
+	error: function(response) {
+		console.log(response);
+	}
+})
